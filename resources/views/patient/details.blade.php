f2171ac8c35ae3e1e5978b746d59c0d4a00b9844
charles <charles@mutuallyassured.com>
2025-06-24 23:56:31 -0700

    Added component

--- resources/views/patient/details.blade.php
+++ resources/views/patient/details.blade.php
@@ -1,6 +1,4 @@
-<div>
-    <h1>{{ $patient->full_name }} (#{{ $patient->id }})</h1>
-    <p>{{ $patient->dob }} - {{ $patient->status }}</p>
-    <p>{{ $patient->species }} {{ $patient->gender }}</p>
-    <p>{{ $patient->email }}</p>
-</div>
+@extends("app")
+@section("content")
+    <x-patient.details :patient="$patient" />
+@endsection
