ccd3b59d409f80acce3153f0ca2c199388a5e7e4
charles <charles@mutuallyassured.com>
2025-06-25 21:10:24 -0700

    Fleshing out the search a bit better.

--- resources/views/components/patient/details.blade.php
+++ resources/views/components/patient/details.blade.php
@@ -7,12 +7,13 @@
                 alt="Bleh"
             />
             <div class="w-full">
+                <a href="{{ route('patient.details', $patient) }}">
                 <h2 class="font-extrabold text-zinc-700">{{ $patient->full_name }} - (#{{ $patient->id }})</h2>
+                </a>
                 <p class="text-sm mb-1">{{ $patient->dob }} - {{ $patient->status }}</p>
                 <p class="text-sm mb-1">{{ $patient->species }} {{ $patient->gender }}</p>
                 <p class="text-sm mb-1">{{ $patient->email }}</p>
             </div>
         </div>
     </x-slot:content>
-
 </x-elements.card>
