ec49aac3f4f904204e9f554d64cc84f31480b4ab
charles <charles@mutuallyassured.com>
2025-06-26 01:17:29 -0700

    Added EnumToArray and PatientStatus traits

---
+++ app/Enum/PatientStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum PatientStatus: string
+{
+    use EnumToArray;
+
+    case Active = 'Active';
+    case Inactive = 'Inactive';
+    case Prospective = 'Prospective';
+    case Archived = 'Archived';
+}
